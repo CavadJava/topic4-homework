8b9ae9e63e28d7a328477e7f88d105723697e535
javad <j.bagirov@expressbank.az>
2025-09-10 15:59:04 +0400

    Updated js,added error line

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,50 +9,72 @@ function viewTransactionHistoryBtn () {
     }
 }
 
-function btnWithDraw(){
+function isInsufficientBalance(balance=0) {
+    if(balance>9){
+        document.querySelector(".error-message").textContent = "";
+        document.querySelector('.btn-withdraw').disabled = false;
+        return false;
+    }else{
+        document.querySelector(".error-message").textContent = "Insufficient balance";
+        document.querySelector(".error-message").style.color = "red";
+        document.querySelector('.btn-withdraw').disabled = true;
+        return true;
+    }
+}
+
+function withDraw(){
     var inputAmount = document.querySelector("#current-balance").value;
-    if(inputAmount==0){
+    if(inputAmount<=0){
         return
     }
-    var spanAmount = document.querySelector("span#span-current-balance").textContent;
-    if(spanAmount==0){
+    var balance = document.querySelector("span#span-current-balance").textContent;
+    if(balance<=0){
         return
+    }
+    if(isInsufficientBalance(Number(balance)-Number(inputAmount))){
+        return;
     }
 
-    document.querySelector("#span-current-balance").textContent = Number(spanAmount)-Number(inputAmount);
+    document.querySelector("#span-current-balance").textContent = Number(balance)-Number(inputAmount);
     var type = "withdraw";
     data.push({amount: inputAmount, type: type, date: new Date()});
     document.querySelector("tbody#transaction-history-rows").appendChild(createRow(type, inputAmount, "red"));
 }
 
-function btnDeposit(){
+function deposit(){
     var inputAmount = document.querySelector("#current-balance").value;
-    var spanAmount = document.querySelector("span#span-current-balance").textContent;
+    var balance = document.querySelector("span#span-current-balance").textContent;
     if(inputAmount==0){
         return
     }
-    if(spanAmount==0 && inputAmount==0){
+    if(balance==0 && inputAmount==0){
         return
     }
 
-    document.querySelector("#span-current-balance").textContent = Number(spanAmount)+Number(inputAmount);
+    document.querySelector("#span-current-balance").textContent = Number(balance)+Number(inputAmount);
     var type = "deposit";
     data.push({amount: inputAmount, type: type, date: new Date()});
     document.querySelector("tbody#transaction-history-rows").appendChild(createRow(type, inputAmount));
+    document.querySelector(".error-message").textContent = "";
+    document.querySelector('.btn-withdraw').disabled = false;
 }
 
 function createRow(type, inputAmount=0, _style='green'){
     let row = document.createElement("tr");
     let date = new Date();
     row.innerHTML = "<td>" + date.getDate() + "-" + (date.getMonth()+1) + "-" + date.getFullYear() +
-        "</td><td class='text-amount' style='background-color: " + _style + "'>" + type +
+        "</td><td class='text-amount' style='color: " + _style + "'>" + type +
         "</td><td>" + inputAmount +
         "</td>";
     return row;
 }
 
-function btnClear(){
+function clear(){
     data = [];
     document.querySelector("span#span-current-balance").textContent = 2000;
     document.querySelector("tbody#transaction-history-rows").innerHTML = "";
+    document.querySelector(".error-message").textContent = "";
+    document.querySelector(".error-message").style.color = "black";
+    document.querySelector(".error-message").style.backgroundColor = "white";
+    document.querySelector('.btn-withdraw').disabled = false;
 }
